57d89e90cbe342213bc506c54289ab49c75bea8b
Dion <your-github-villamordion417@gmail.com>
2026-03-21 22:17:40 +0900

    Fixed app path in INSTALLED_APPS

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'motivational_quotes',
+    'mysite.motivational_quotes',
 ]
 
 MIDDLEWARE = [
