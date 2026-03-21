d98d48d00a02d2f603d8f98c2173326881c2d85f
Dion <your-github-villamordion417@gmail.com>
2026-03-21 22:12:07 +0900

    Added app to INSTALLED_APPS and enabled WhiteNoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5jy3y-b)uvnzxc+wcv8njaypn%l8ms-0-&1+su3#h-bn(u=f(d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['daily-motivational-quote-10.onrender.com']
+ALLOWED_HOSTS = ['daily-motivational-quote-10.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'motivational_quotes',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
